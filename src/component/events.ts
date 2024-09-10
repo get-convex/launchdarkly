ad2a1ec261942b2c6a4237746e0278db3dc45d96
Ari Trakhtenberg <ari@convex.dev>
2024-09-10 13:59:53 -0700

    remove commented files

--- src/component/events.ts
+++
@@ -1,9 +0,0 @@
-// import { v } from "convex/values";
-// import { mutation } from "./_generated/server";
-
-// export const storeEvent = mutation({
-//   args: { payload: v.string() },
-//   handler: async (ctx, { payload }) => {
-//     await ctx.db.insert("events", { payload });
-//   },
-// });
