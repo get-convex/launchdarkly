2df3a83dfcaa946f5cf464e709b496bb99d2f277
Ari Trakhtenberg <ari@convex.dev>
2024-09-10 14:00:15 -0700

    Revert "remove commented files"
    This reverts commit ad2a1ec261942b2c6a4237746e0278db3dc45d96.

---
+++ src/component/events.ts
@@ -0,0 +1,9 @@
+// import { v } from "convex/values";
+// import { mutation } from "./_generated/server";
+
+// export const storeEvent = mutation({
+//   args: { payload: v.string() },
+//   handler: async (ctx, { payload }) => {
+//     await ctx.db.insert("events", { payload });
+//   },
+// });
