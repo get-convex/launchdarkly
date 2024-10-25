efc347b29b708111a3bbdc65e3219e23d8e96896
Ari <ari@convex.dev>
2024-10-25 15:43:49 -0700

    Address review

--- example/convex/example.ts
+++ example/convex/example.ts
@@ -2,14 +2,14 @@ import { LaunchDarkly } from "@convex-dev/launchdarkly";
 import { mutation, query } from "./_generated/server";
 import { components } from "./_generated/api";
 
-const ld = new LaunchDarkly(components.launchdarkly);
+const launchdarkly = new LaunchDarkly(components.launchdarkly);
 
 export const listFruits = query({
   handler: async (ctx) => {
-    const launchdarkly = ld.sdk(ctx);
+    const ld= = launchdarkly.sdk(ctx);
     const user = { key: "myUserId" };
 
-    const showFruits = await launchdarkly.boolVariation(
+    const showFruits = await ld.boolVariation(
       "can-show-fruits",
       user,
       true
@@ -23,11 +23,11 @@ export const listFruits = query({
 
 export const buyFruit = mutation({
   handler: async (ctx) => {
-    const launchdarkly = ld.sdk(ctx);
+    const ld = launchdarkly.sdk(ctx);
 
     const user = { key: "myUserId" };
 
-    const showFruits = await launchdarkly.boolVariation(
+    const showFruits = await ld.boolVariation(
       "can-buy-fruits",
       user,
       false
@@ -39,7 +39,7 @@ export const buyFruit = mutation({
       };
     }
 
-    launchdarkly.track("buy-fruit", user);
+    ld.track("buy-fruit", user);
   },
 });
 
@@ -49,9 +49,9 @@ export const initialized = query({
     if (!process.env.LAUNCHDARKLY_SDK_KEY) {
       return false;
     }
-    const launchdarkly = ld.sdk(ctx);
+    const ld = launchdarkly.sdk(ctx);
     return (
-      (await launchdarkly.allFlagsState({ key: "any" })).allValues()[
+      (await ld.allFlagsState({ key: "any" })).allValues()[
         "can-buy-fruits"
       ] !== undefined
     );
