1a86560fe3b376eddbe18dd15f3ed083ce134974
Ian Macartney <ianhmacartney@gmail.com>
2025-11-04 02:41:33 -0800

    update configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    environment: "edge-runtime",
+    typecheck: {
+      tsconfig: "./tsconfig.test.json",
+    },
     coverage: {
       exclude: [...configDefaults.exclude, "example"],
     },
