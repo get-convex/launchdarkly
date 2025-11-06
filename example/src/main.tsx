3ce6be353e90e4a9469d69bf76ed07e24a1f1fa6
Ian Macartney <ianhmacartney@gmail.com>
2025-11-06 00:46:20 -0800

    lint

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import "./index.css";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
@@ -12,5 +12,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </ConvexProvider>
+  </ConvexProvider>,
 );
